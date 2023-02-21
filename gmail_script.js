b7241d88d0918f86b6b35af364e6aae852386f96
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-02-21 13:58:55 -0500

    Added the code that scrapes the gmail for the selected email to which we want to reply to.

---
+++ gmail_script.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    window.onhashchange = () => {
+        if (window.location.hash.startsWith('#inbox/')) {
+            const spans = document.querySelectorAll('span');
+
+            for (const span of spans) {
+                if (span.innerText === 'Reply') {
+                    span.addEventListener('click', function() {
+                        const email = document.querySelector('.adn.ads');
+                        console.log(email.textContent);
+                    });
+                }
+            }
+        }
+    };
+}
